e1b99c52b014df27069f4ca83d0e945614c3f7d1
Tim Seeger <tim.seeger@campus.tu-berlin.de>
2023-12-19 14:44:50 +0100

    Failed to call GetDump service, Inter returned an empty path aka: when you get what you want but not what you need

--- planners/inter/polite_inter/include/polite_inter.h
+++ planners/inter/polite_inter/include/polite_inter.h
@@ -1,88 +1,52 @@
+// polite_inter.h
 #ifndef POLITE_INTER_H_
 #define POLITE_INTER_H_
 
 #include <ros/ros.h>
 #include <mbf_costmap_core/costmap_inter.h>
 #include <boost/thread/mutex.hpp>
-
 #include <dynamic_reconfigure/server.h>
 #include <polite_inter/PoliteInterConfig.h>
+#include <costmap_2d/SemanticDump.h>
+#include <costmap_2d/GetSemanticInfo.h>
+#include <costmap_2d/GetDump.h>  // Add this include for the service definition
 
 namespace polite_inter
 {
 
-    class PoliteInter : public mbf_costmap_core::CostmapInter
-    {
+class PoliteInter : public mbf_costmap_core::CostmapInter
+{
 
-        using mbf_costmap_core::CostmapInter::CostmapInter;
+    using mbf_costmap_core::CostmapInter::CostmapInter;
 
-        /**
-         * @brief Given a goal pose in the world, compute a plan
-         * @param start The start pose
-         * @param goal The goal pose
-         * @param plan The plan... filled by the inter
-         * @param cost The cost for the the plan
-         * @param message Optional more detailed outcome as a string
-         * @return Result code as described on GetInterPath action result:
-         *         SUCCESS         = 0
-         *         1..9 are reserved as plugin specific non-error results
-         *         FAILURE         = 50  # Unspecified failure, only used for old, non-mfb_core based plugins
-         *         CANCELED        = 51
-         *         INVALID_START   = 52
-         *         INVALID_GOAL    = 53
-         *         BLOCKED_START   = 54
-         *         BLOCKED_GOAL    = 55
-         *         NO_PATH_FOUND   = 56
-         *         PAT_EXCEEDED    = 57
-         *         EMPTY_PATH      = 58
-         *         TF_ERROR        = 59
-         *         NOT_INITIALIZED = 60
-         *         INVALID_PLUGIN  = 61
-         *         INTERNAL_ERROR  = 62
-         *         71..99 are reserved as plugin specific errors
-         */
-        uint32_t makePlan(const geometry_msgs::PoseStamped &start, const geometry_msgs::PoseStamped &goal,
-                          std::vector<geometry_msgs::PoseStamped> &plan, double &cost, std::string &message);
+public:
+    uint32_t makePlan(const geometry_msgs::PoseStamped &start,
+                      const geometry_msgs::PoseStamped &goal,
+                      std::vector<geometry_msgs::PoseStamped> &plan,
+                      double &cost,
+                      std::string &message) override;
 
-        /**
-         * @brief  Set the plan that the planner is following
-         * @param plan The plan to pass to the inter
-         * @return True if the plan was updated successfully, false otherwise
-         */
-        bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan);
+    bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan,
+                 const std::vector<costmap_2d::SemanticDump> &semantic_info);
 
-        /**
-         * @brief Requests the inter to cancel, e.g. if it takes too much time.
-         * @remark New on MBF API
-         * @return True if a cancel has been successfully requested, false if not implemented.
-         */
-        bool cancel() { return false; };
+    void initialize(std::string name, costmap_2d::Costmap2DROS *global_costmap_ros,
+                    costmap_2d::Costmap2DROS *local_costmap_ros) override;
 
-        /**
-         * @brief Initialization function for the CostmapInter
-         * @param name The name of this inter
-         * @param costmap_ros A pointer to the ROS wrapper of the costmap to use for planning
-         */
-        void initialize(std::string name, costmap_2d::Costmap2DROS *global_costmap_ros, costmap_2d::Costmap2DROS *local_costmap_ros);
+    bool getSemanticInfo(costmap_2d::GetSemanticInfo::Request &req,
+                         costmap_2d::GetSemanticInfo::Response &res);
 
-    private:
-        // storage for setPlan
-        std::vector<geometry_msgs::PoseStamped> plan_;
-        boost::mutex plan_mtx_;
+private:
+    dynamic_reconfigure::Server<polite_inter::PoliteInterConfig> server;
+    std::vector<geometry_msgs::PoseStamped> plan_;
+    std::vector<costmap_2d::SemanticDump> semantic_info_;
+    boost::mutex plan_mtx_;
+    boost::mutex vision_cfg_mtx_;
+    double vision_limit_ = 0.0;
+    size_t min_poses_ = 0;
 
-        // could be used for nh
-        std::string name;
+    void reconfigure(polite_inter::PoliteInterConfig &config, uint32_t level);
+};
 
-        // max vision
-        double vision_limit_ = 0.2;
-
-        // min poses in path
-        size_t min_poses_ = 1;
-
-        boost::mutex vision_cfg_mtx_;
-
-        void reconfigure(polite_inter::PoliteInterConfig &config, uint32_t level);
-    };
-}
+} // namespace polite_inter
 
 #endif // POLITE_INTER_H_
--- planners/inter/polite_inter/src/polite_inter.cpp
+++ planners/inter/polite_inter/src/polite_inter.cpp
@@ -1,82 +1,94 @@
+// polite_inter.cpp
 #include "../include/polite_inter.h"
 #include <pluginlib/class_list_macros.hpp>
-#include <mbf_costmap_core/mbf_costmap_core.h> // Include the mbf_costmap_core header
-#include <ros/service.h>
 #include <costmap_2d/costmap_2d_publisher.h>
-#include <costmap_2d/cost_values.h>
 
-namespace polite_inter {
+PLUGINLIB_EXPORT_CLASS(polite_inter::PoliteInter, mbf_costmap_core::CostmapInter)
 
+namespace polite_inter
+{
 uint32_t PoliteInter::makePlan(const geometry_msgs::PoseStamped &start,
                                const geometry_msgs::PoseStamped &goal,
                                std::vector<geometry_msgs::PoseStamped> &plan,
-                               double &cost, std::string &message) {
-  // Extract information from the semantic layer service
-  costmap_2d::GetDump srv;
-  if (!ros::service::call("/semantic_layer_namespace/get_dump", srv)) {
-    ROS_ERROR("Failed to call semantic layer service");
-    return mbf_costmap_core::INTERNAL_ERROR;
-  }
+                               double &cost,
+                               std::string &message)
+{
+    boost::unique_lock<boost::mutex> lock(vision_cfg_mtx_);
+    boost::unique_lock<boost::mutex> lock2(plan_mtx_);
 
-  // Process the semantic layer information
-  const auto &semanticLayers = srv.response.semantic_layers;
-  for (const auto &semanticLayer : semanticLayers.layers) {
-    // Extract information relevant to your logic from semanticLayer
-    // Implement your logic to determine if an object is 2 meters or nearer
-    // You may need to consider the transformation between the semantic layer and the robot's frame.
-    // For simplicity, let's assume there is a function getObjectDistance() that extracts the distance.
-    double objectDistance = getObjectDistance(semanticLayer);
+    // Check for obstacles in the semantic layer within 2 meters of the robot
+    for (const auto &semantic : semantic_info_)
+    {
+        double distance = std::hypot(start.pose.position.x - semantic.x, start.pose.position.y - semantic.y);
 
-    if (objectDistance <= 2.0) {
-      // Stop the robot
-      plan.clear();
-      cost = 0.0;
-      message = "Obstacle detected within 2 meters. Stopping the robot.";
-      return mbf_costmap_core::SUCCESS;
+        if (distance <= 2.0)
+        {
+            // Obstacle within 2 meters, stop the robot
+            plan.clear();
+            return 0; // Success, but the plan is empty
+        }
     }
-  }
 
-  // Your existing planning logic here...
+    // If no obstacle within 2 meters, proceed with the original behavior
+    size_t limit = std::floor(plan_.size() * vision_limit_);
+    limit = std::max(limit, min_poses_);
+    limit = std::min(limit, plan_.size());
 
-  // Populate the plan with a simple straight line for demonstration purposes.
-  plan.clear();
-  plan.push_back(start);
-  plan.push_back(goal);
+    plan = std::vector<geometry_msgs::PoseStamped>(plan_.begin(), plan_.begin() + limit);
 
-  cost = 0.0; // Set the cost as needed
-
-  return mbf_costmap_core::SUCCESS;
+    return 0;
 }
 
-double PoliteInter::getObjectDistance(const pedsim_msgs::SemanticData &semanticLayer) {
-  // Implement the logic to extract distance information from semanticLayer
-  // Return the distance value
-  // Example: return semanticLayer.distance;
-  return 0.0; // Replace with actual logic
+bool PoliteInter::setPlan(const std::vector<geometry_msgs::PoseStamped> &plan,
+                          const std::vector<costmap_2d::SemanticDump> &semantic_info)
+{
+    boost::unique_lock<boost::mutex> lock(plan_mtx_);
+    plan_ = plan;
+    semantic_info_ = semantic_info;
+    return true;
 }
 
-bool PoliteInter::setPlan(const std::vector<geometry_msgs::PoseStamped> &plan) {
-  boost::unique_lock<boost::mutex> lock(plan_mtx_);
-  plan_ = plan;
-  return true;
+void PoliteInter::initialize(std::string name, costmap_2d::Costmap2DROS *global_costmap_ros,
+                             costmap_2d::Costmap2DROS *local_costmap_ros)
+{
+    this->name = name;
+
+    dynamic_reconfigure::Server<polite_inter::PoliteInterConfig> server;
+    server.setCallback(boost::bind(&PoliteInter::reconfigure, this, _1, _2));
+
+    // Add a service server for getting semantic information
+    ros::NodeHandle nh("~");
+    ros::ServiceServer service = nh.advertiseService("get_semantic_info", &PoliteInter::getSemanticInfo, this);
 }
 
-void PoliteInter::initialize(std::string name,
-                              costmap_2d::Costmap2DROS *global_costmap_ros,
-                              costmap_2d::Costmap2DROS *local_costmap_ros) {
-  this->name = name;
+bool PoliteInter::getSemanticInfo(costmap_2d::GetSemanticInfo::Request &req,
+                                  costmap_2d::GetSemanticInfo::Response &res)
+{
+    costmap_2d::GetDump srv;
+    ros::NodeHandle nh("~");
 
-  dynamic_reconfigure::Server<polite_inter::PoliteInterConfig> server;
-  server.setCallback(boost::bind(&PoliteInter::reconfigure, this, _1, _2));
+    // Assuming semantic layer service is named "semantic_layer/get_dump"
+    ros::ServiceClient client = nh.serviceClient<costmap_2d::GetDump>("semantic_layer/get_dump");
+
+    if (client.call(srv))
+    {
+        res.semantic_info = srv.response.semantic_layers;
+
+        // Additional processing if needed
+
+        return true;
+    }
+    else
+    {
+        ROS_ERROR("Failed to call semantic layer service");
+        return false;
+    }
+}
+void PoliteInter::reconfigure(polite_inter::PoliteInterConfig &config, uint32_t level)
+{
+    boost::unique_lock<boost::mutex> lock(vision_cfg_mtx_);
+    vision_limit_ = config.vision_limit;
+    min_poses_ = config.min_poses;
 }
 
-void PoliteInter::reconfigure(polite_inter::PoliteInterConfig &config,
-                               uint32_t level) {
-  boost::unique_lock<boost::mutex> lock(vision_cfg_mtx_);
-  vision_limit_ = config.vision_limit;
-  min_poses_ = config.min_poses;
-}
-
-}  // namespace polite_inter
-
-PLUGINLIB_EXPORT_CLASS(polite_inter::PoliteInter, mbf_costmap_core::CostmapInter)
+} // namespace polite_inter
