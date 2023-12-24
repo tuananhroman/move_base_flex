619785ea095dbf4af0f22f815db07c53dabd4fb3
AhmedMartban <ahmed_martban@hotmail.de>
2023-12-24 22:02:32 +0100

    add angle threshhold and relative angle calculation

--- planners/inter/sideways_inter/include/sideways_inter.h
+++ planners/inter/sideways_inter/include/sideways_inter.h
@@ -66,6 +66,13 @@ namespace sideways_inter
          */
         void initialize(std::string name, costmap_2d::Costmap2DROS *global_costmap_ros, costmap_2d::Costmap2DROS *local_costmap_ros);
 
+
+
+            /**
+         * @brief Makes the robot move back a specified distance
+         * @param distance the distance in emtres we want the robot to moev back
+         */
+        
     private:
         // storage for setPlan
         ros::ServiceClient get_dump_client_;
@@ -77,6 +84,8 @@ namespace sideways_inter
 
         // max vision
         double vision_limit_ = 0.2;
+
+        ros::NodeHandle nh_;
 
         // min poses in path
         size_t min_poses_ = 1;
--- planners/inter/sideways_inter/src/sideways_inter.cpp
+++ planners/inter/sideways_inter/src/sideways_inter.cpp
@@ -3,119 +3,119 @@
 #include <costmap_2d/GetDump.h>
 #include <costmap_2d/costmap_2d_publisher.h>
 #include <pluginlib/class_list_macros.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 PLUGINLIB_EXPORT_CLASS(sideways_inter::sidewaysInter, mbf_costmap_core::CostmapInter)
-
 
 namespace sideways_inter
 {
 
+    ros::ServiceClient get_dump_client_;
     const uint32_t SUCCESS = 0;
     const uint32_t INTERNAL_ERROR = 1;
+    geometry_msgs::PoseStamped temp_goal;
+    bool new_goal_set_ = false;
+    double distance_threshold = 4.0;  // Setze die Distanzschwelle nach Bedarf
+    double angle_threshold = 2* M_PI ;  // Setze die Winkeltoleranz nach Bedarf
+    
 
     uint32_t sidewaysInter::makePlan(const geometry_msgs::PoseStamped &start, const geometry_msgs::PoseStamped &goal,
-                                  std::vector<geometry_msgs::PoseStamped> &plan, double &cost, std::string &message)
+                                std::vector<geometry_msgs::PoseStamped> &plan, double &cost, std::string &message)
     {
-        // Call the GetDump service to obtain semantic layers
-        costmap_2d::GetDump getDump_srv;
+        // Create a request message
+        //costmap_2d::GetDump::Request request;
+        // No need to set any specific fields in the request for this example
+
+        // Create a response message
+        //costmap_2d::GetDump::Response response;
+
+        costmap_2d::GetDump srv;
+        // Lock the mutex for plan_
+        boost::unique_lock<boost::mutex> lock(plan_mtx_);
+        
+        double robot_x = start.pose.position.x;
+        double robot_y = start.pose.position.y;
+        //ROS_ERROR("Original Goal at the Start: x: %f, y: %f, z: %f, orientation: %f",
+        //            goal.pose.position.x, goal.pose.position.y, goal.pose.position.z, tf::getYaw(goal.pose.orientation));
 
         // Call the GetDump service
-        
-        if (get_dump_client_.call(getDump_srv))
+        if (get_dump_client_.call(srv))
         {
-            ROS_INFO("We called the service");
+            //ROS_ERROR("GetDump service call successful");
 
             // Access the semantic layers from the response
-            auto semantic_layers = getDump_srv.response.semantic_layers;
+            auto semantic_layers = srv.response.semantic_layers;
 
             // Process the semantic layers
             for (const auto &semantic_layer : semantic_layers)
             {
-                ROS_INFO("Semantic Layer Names:");
+                //ROS_ERROR("Semantic Layer Names:");
 
                 // Iterate through the layers
                 for (const auto &layer : semantic_layer.layers)
                 {
-                    ROS_INFO("Layer Name: %s", layer.type.c_str());
+                    //ROS_ERROR("Layer Name: %s", layer.type.c_str());
 
-
-
-
-                    if (strcmp(layer.type.c_str(), "pedestrian") == 0)
+                    // Iterate through the points in each layer
+                    if (layer.type == "pedestrian")
                     {
-                     // Iterate through the points in each layer
-                    for (const auto &point : layer.points)
-                    {
-                                                                                                                                         //debug to the position of pedestrians :ROS_INFO("Location: x: %f, y: %f, z: %f", point.location.x, point.location.y, point.location.z);
+                        for (const auto &point : layer.points)
+                        {
+                            double distance = std::sqrt(std::pow(point.location.x - robot_x, 2) + std::pow(point.location.y - robot_y, 2));
 
-                    //TODO: When the robot sees a pedestrian he should stop
-                    // Calculate the distance between the robot and the pedestrian
-                    double distance = std::sqrt(std::pow(start.pose.position.x - point.location.x, 2) +
-                                                std::pow(start.pose.position.y - point.location.y, 2));
+                            double relative_x = point.location.x - robot_x;
+                            double relative_y = point.location.y - robot_y;                            
 
-                    ROS_ERROR("Distance to pedestrian: %f meters", distance);                                                            // debug to see position and goal: ROS_ERROR("Current Position: %.2f, %.2f, Goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y, goal.pose.position.x, goal.pose.position.y);
-                    
-                    if (distance <= 4.0){
-                        ROS_ERROR("Pedestrian too close! Stopping and moving backward.");
+                            //ROS_ERROR("Location: x: %f, y: %f, z: %f, Distance: %f", point.location.x, point.location.y, point.location.z, distance);
+                            // Check if the pedestrian is 2 meters or nearer (adjust to desired distance)
+                            if ((relative_y > 0) && (distance <= distance_threshold) && !new_goal_set_)
+                            {
+                            // Berechne den Winkel zwischen der aktuellen Ausrichtung des Roboters und der relativen Position des Fußgängers
+                                double angle_to_pedestrian = std::atan2(relative_y, relative_x); 
+                                //ROS_ERROR("Condition Satisfied. Distance: %f", distance);
+                                if(!new_goal_set_ && std::abs(angle_to_pedestrian) < angle_threshold){
+                                    //ROS_ERROR("Setting new goal");
+                                    temp_goal = start;
 
-                        // Calculate the angle between the current position and the goal
-                        double angle = std::atan2(goal.pose.position.y - start.pose.position.y, goal.pose.position.x - start.pose.position.x);
+                                    double theta = tf::getYaw(temp_goal.pose.orientation);
+                                    //double sideways_angle = theta + (M_PI / 2.0);  // Winkel um π/2 drehen
+                                    temp_goal.pose.position.x -= 1.0 * cos(theta);
+                                    temp_goal.pose.position.y -= 1.0 * sin(theta);
+                                   
 
-                        // Calculate the new goal position 2 meters backward
-                        geometry_msgs::PoseStamped new_goal;
-                        new_goal.pose.position.x = start.pose.position.x - 2.0 * cos(angle);
-                        new_goal.pose.position.y = start.pose.position.y - 2.0 * sin(angle);
-
-                        // Update the plan with the new goal
-                        plan.clear();
-                        plan.push_back(new_goal);
-
-                        // Output the updated plan to the console
-                        ROS_ERROR("Updated Plan:");
-
-                        for (const auto &pose : plan) {
-                            ROS_ERROR("Plan Pose: x=%.2f, y=%.2f", pose.pose.position.x, pose.pose.position.y);
-                            
-                        }                       
-
-                        // Set the cost to a high value to discourage the robot from taking this path
-                        cost = std::numeric_limits<double>::infinity();
-                    }                    
-                  
+                                    temp_goal.pose.orientation = tf::createQuaternionMsgFromYaw(tf::getYaw(temp_goal.pose.orientation));
+                                    temp_goal.header.frame_id = start.header.frame_id;
+                                    new_goal_set_ = true;
+                                }
+                            }
+                        }
+                    }
                 }
-                }
-                }
-                // Check if the layer is of type "pedestrian" and adjust the plan accordingly
-
             }
+            if (new_goal_set_)
+            {
+                double distance_to_temp_goal = std::sqrt(std::pow(temp_goal.pose.position.x - robot_x, 2) + std::pow(temp_goal.pose.position.y - robot_y, 2));
+                
+                if (distance_to_temp_goal <= 0.2) // Adjust the threshold as needed
+                {
+                    ROS_ERROR("Reached temp_goal. Resetting goal.");
+                    new_goal_set_ = false;
+                }
+                //ROS_ERROR("Setting new goal");
+                //ROS_ERROR("Position: x = %f, y = %f, z = %f", temp_goal.pose.position.x, temp_goal.pose.position.y, temp_goal.pose.position.z);
 
-            
-            //TODO: Use semantic_layers data to modify the plan as needed
-
-            // Lock the mutexes for plan_ and vision_cfg_mtx_
-            boost::unique_lock<boost::mutex> lock2(plan_mtx_);
-            boost::unique_lock<boost::mutex> lock(vision_cfg_mtx_);
-
-            // Modify the plan based on the semantic_layers data
-            size_t limit = std::floor(plan_.size() * vision_limit_);
-            limit = std::max(limit, min_poses_);
-            limit = std::min(limit, plan_.size());
-
-            plan = std::vector<geometry_msgs::PoseStamped>(plan_.begin(), plan_.begin() + limit);
-
-            // Unlock the mutexes
-            lock.unlock();
-            lock2.unlock();
-
-            // Return the result code
+                // Clear the existing plan and add temp_goal
+                plan.clear();
+                plan.push_back(temp_goal);
+                return 0;
+            }
+            plan.insert(plan.end(), plan_.begin(), plan_.end());
             return 0;
         }
-        
         else
         {
-            ROS_ERROR_STREAM("Failed to call GetDump service. Call result: " << get_dump_client_.call(getDump_srv));
-          
-            // Handle the error and return an appropriate result code
+            ROS_ERROR("Failed to call GetDump service");
             return sideways_inter::INTERNAL_ERROR;
         }
     }
@@ -131,9 +131,11 @@ namespace sideways_inter
     {
         this->name = name;
 
+        nh_ = ros::NodeHandle("~");
+
         // Create a service client for the GetDump service
-        get_dump_client_ = ros::NodeHandle("~").serviceClient<costmap_2d::GetDump>("global_costmap/get_dump");
-     
+        get_dump_client_ = nh_.serviceClient<costmap_2d::GetDump>("global_costmap/get_dump");
+    
         dynamic_reconfigure::Server<sideways_inter::sidewaysInterConfig> server;
         server.setCallback(boost::bind(&sidewaysInter::reconfigure, this, _1, _2));
     }
@@ -141,8 +143,6 @@ namespace sideways_inter
     void sidewaysInter::reconfigure(sideways_inter::sidewaysInterConfig &config, uint32_t level)
     {
         boost::unique_lock<boost::mutex> lock(vision_cfg_mtx_);
-        vision_limit_ = config.vision_limit;
         min_poses_ = config.min_poses;
     }
-
 }
